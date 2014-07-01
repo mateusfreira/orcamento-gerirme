76227f8f0abe04d4390735201ee3e972d8ac5d68
Mateus Freira <mateus.freira@gmail.com>
2014-07-01 18:17:36 -0300

    migrtation to androidStudio, removing account,

--- src/com/gerime/activity/RegisterActivity.java
+++ src/com/gerime/activity/RegisterActivity.java
@@ -12,66 +12,77 @@ import com.gerime.model.AccountMoviment;
 import com.gerime.repository.EntityManager;
 
 public class RegisterActivity extends Activity {
-	AccountMoviment accountMoviment;
+    AccountMoviment accountMoviment;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_register);
-		loadData();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        loadData();
+    }
 
-	private void loadData() {
-		accountMoviment = new AccountMoviment();
-		try {
-			long id = getIntent().getExtras().getLong("id");
-			accountMoviment.setId(id);
-			accountMoviment = EntityManager.getSqlAdapter().findFirst(
-					accountMoviment);
-			((EditText) findViewById(R.id.value)).setText(Float
-					.toString(accountMoviment.getValue()));
-			((EditText) findViewById(R.id.description)).setText(accountMoviment
-					.getDescription());
-			((EditText) findViewById(R.id.date)).setText(accountMoviment
-					.getDate());
+    private void loadData() {
+        accountMoviment = new AccountMoviment();
+        try {
+            long id = getIntent().getExtras().getLong("id");
+            accountMoviment.setId(id);
+            accountMoviment = EntityManager.getSqlAdapter().findFirst(
+                    accountMoviment);
+            ((EditText) findViewById(R.id.value)).setText(Float
+                    .toString(accountMoviment.getValue()));
+            ((EditText) findViewById(R.id.description)).setText(accountMoviment
+                    .getDescription());
+            ((EditText) findViewById(R.id.date)).setText(accountMoviment
+                    .getDate());
 
-			if (accountMoviment.isIn()) {
-				((Switch) findViewById(R.id.type)).setChecked(true);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            if (accountMoviment.isIn()) {
+                ((Switch) findViewById(R.id.type)).setChecked(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.register, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.register, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-		if (item.getItemId() == R.id.save) {
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        boolean result;
+        switch (item.getItemId()) {
+            case R.id.save:
+                saveAccount();
+                result = true;
+                break;
+            case R.id.remove:
+                EntityManager.getSqlAdapter().delete(accountMoviment);
+                result = true;
+                break;
+            default:
+                result = super.onMenuItemSelected(featureId, item);
+        }
+        return result;
 
-			accountMoviment.setAccount(1);
-			accountMoviment.setValue(Float
-					.parseFloat(((EditText) findViewById(R.id.value)).getText()
-							.toString()));
+    }
 
-			accountMoviment.setType(((Switch) findViewById(R.id.type))
-					.isChecked() ? AccountMoviment.IN : AccountMoviment.OUT);
+    private void saveAccount() {
+        accountMoviment.setAccount(1);
+        accountMoviment.setValue(Float
+                .parseFloat(((EditText) findViewById(R.id.value)).getText()
+                        .toString()));
 
-			accountMoviment
-					.setDescription(((EditText) findViewById(R.id.description))
-							.getText().toString());
-			accountMoviment.setDate(((EditText) findViewById(R.id.date))
-					.getText().toString());
-			EntityManager.getSqlAdapter().store(accountMoviment);
-			finish();
-			return true;
-		} else {
-			return super.onMenuItemSelected(featureId, item);
-		}
+        accountMoviment.setType(((Switch) findViewById(R.id.type))
+                .isChecked() ? AccountMoviment.IN : AccountMoviment.OUT);
 
-	}
+        accountMoviment
+                .setDescription(((EditText) findViewById(R.id.description))
+                        .getText().toString());
+        accountMoviment.setDate(((EditText) findViewById(R.id.date))
+                .getText().toString());
+        EntityManager.getSqlAdapter().store(accountMoviment);
+        finish();
+    }
 }
